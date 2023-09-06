d3151b626de6c42c3a948b538080db41bec18900
Carlos Daniel <carlosdalves.ads@gmail.com>
2023-09-05 23:23:29 -0300

    Projeto Final

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
-    list_display_links = ("id", "nome")
-    search_fields = ("nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada") # Colunas da lista
+    list_display_links = ("id", "nome") # Onde os links de edição aparecem
+    search_fields = ("nome",) # Campos filtráveis
+    list_filter = ("categoria",) # Categorias filtráveis
+    list_editable = ("publicada", ) # Celulas que podem ser editáveis
+    list_per_page = 10 # itens por página
 
 
 admin.site.register(Fotografia, ListandoFotografias)
